390dde332c7b1a2a3ba34c1796fc509f60b02ba6
hardrockhodl <105864199+hardrockhodl@users.noreply.github.com>
2025-10-02 14:50:15 +0200

    Updated useTheme.ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -10,6 +10,13 @@ export function useTheme() {
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
+
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+
     localStorage.setItem('theme', theme);
   }, [theme]);
 
